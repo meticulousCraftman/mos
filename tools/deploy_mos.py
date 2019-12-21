0211d491e24fb5e1e0d5694f89a1f1a37fb5c50f
Deomid "rojer" Ryabkov <rojer@rojer.me>
2019-12-21 21:37:59 +0000

    Common Makefile for mos, fwbuild-manager and fwbuild-instance

--- tools/deploy_mos.py
+++ tools/deploy_mos.py
@@ -207,12 +207,12 @@ if __name__ == "__main__":
     if platform == "mac":
         if args.resume <= 10:
             print("(10) Deploying binaries...")
-            RunSubprocess(["make", "-C", "mos", "deploy-mos-binary", "TAG=%s" % tag_effective])
+            RunSubprocess(["make", "deploy-downloads", "TAG=%s" % tag_effective])
         UpdateHomebrew(args)
 
     if args.resume <= 40:
         print("(40) Deploying fwbuild...")
-        RunSubprocess(["make", "-C", "mos", "deploy-fwbuild", "TAG=%s" % tag_effective])
+        RunSubprocess(["make", "deploy-fwbuild", "TAG=%s" % tag_effective])
 
     if args.resume <= 50:
         print("(50) Building Ubuntu packages...")
@@ -225,5 +225,5 @@ if __name__ == "__main__":
         print("""
     ============ WARNING ============
     You're not running on mac, so I couldn't deploy mos binary. You need to do that from mac:
-    $ make -C mos deploy-mos-binary TAG=%s
+    $ make deploy-downloads TAG=%s
     =================================""" % tag_effective)
